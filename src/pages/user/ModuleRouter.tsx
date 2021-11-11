a36fb34e3585b7abfe4a06692554aedf848676f8
shijie <511271085@qq.com>
2021-11-11 10:11:26 +0800

    init

--- src/pages/user/ModuleRouter.tsx
+++ src/pages/user/ModuleRouter.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Switch, Route, Redirect, useRouteMatch } from '@friday/router'
 import Login from 'src/pages/admin/login'
+import PrivateRoute from './PrivateRoute'
 
 const ModuleRouter = () => {
 	const match = useRouteMatch()
     return (
 		<Switch>
             <Route path={`${match.path}/login`} exact component={Login} />
-            
+            <PrivateRoute path={`${match.path}`} component={} />
         </Switch>
     )
 }
