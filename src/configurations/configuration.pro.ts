4a9f9604d0d1d1f1890a6c5cd60bec02f4b91ae6
baishijie <511271085@qq.com>
2021-11-25 10:21:14 +0800

    build

--- src/configurations/configuration.pro.ts
+++ src/configurations/configuration.pro.ts
@@ -5,7 +5,9 @@ import combineUrl from './combineUrl'
 const configuration: IConfiguration = {
 	whiteHosts: [
 		'localhost:3009',
-		'101.42.95.249:9005'
+		'101.42.95.249:9005',
+		'test.ruien.work',
+		'test.ruien.work:9005'
 	],
 	publicUrl: {
 		OPEN_API_URL: combineUrl(upstreams.production.OPEN_API_URL, BaseUrl.OPEN_API_URL),
