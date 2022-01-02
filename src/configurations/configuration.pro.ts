a8331603142318410631af4e9c28067662e1113a
baishijie <511271085@qq.com>
2022-01-02 17:16:00 +0800

    build

--- src/configurations/configuration.pro.ts
+++ src/configurations/configuration.pro.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-04 17:46:21
+ * @LastEditTime: 2022-01-02 17:15:20
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /applyweb/src/configurations/configuration.pro.ts
+ */
 import { IConfiguration } from '@friday/core'
 import { upstreams, BaseUrl, protocol } from './constants'
 import combineUrl from './combineUrl'
@@ -9,6 +17,8 @@ const configuration: IConfiguration = {
 		'test.ruien.work',
 		'test.ruien.work:9005',
 		'101.35.52.7',
+		'joinust.hoo-woo.cn',
+		'joinus.hoo-woo.cn'
 	],
 	publicUrl: {
 		OPEN_API_URL: combineUrl(upstreams.production.OPEN_API_URL, BaseUrl.OPEN_API_URL),
